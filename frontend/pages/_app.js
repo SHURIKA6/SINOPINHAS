b1f9313b57db3644d4c5a2c30e29b87f1abf05e2
SHURIKA6 <fernandoriaddasilvaribeiro@gmail.com>
2025-11-27 13:31:26 -0400

    adicionado speed insights

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,1 @@
-import { SpeedInsights } from '@vercel/speed-insights/react';
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <SpeedInsights />
-    </>
-  );
-}
+import { Analytics } from "@vercel/analytics/next"
